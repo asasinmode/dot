561db26bde57060aaa4dfb9417b2f7938a62c4ac
asasinmode <skin9470@gmail.com>
2023-03-16 21:56:28 +0100

    add wezterm

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,15 @@
+local wezterm = require('wezterm')
+
+return {
+	font = wezterm.font({
+		family = 'FiraCode Nerd Font',
+		harfbuzz_features = { 'cv02=1', 'ss03=1', 'ss05=1' }
+	}),
+	font_size = 18.0,
+	native_macos_fullscreen_mode = true,
+	hide_tab_bar_if_only_one_tab = true,
+	window_background_image = wezterm.home_dir .. '/Documents/terminal.jpg',
+	window_background_image_hsb = {
+		brightness = 0.05
+	}
+}
