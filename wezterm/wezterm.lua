881d5b7bc647e40b3659576aef280447ad350b43
asasinmode <skin9470@gmail.com>
2025-01-31 14:53:38 +0100

    feat: wezterm tweaks & keybinds

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -5,12 +5,12 @@ return {
 		family = "FiraCode Nerd Font",
 		harfbuzz_features = { "cv02=1", "ss03=1", "ss05=1" },
 	}),
-	font_size = 18.0,
+	font_size = 21.0,
 	native_macos_fullscreen_mode = true,
 	hide_tab_bar_if_only_one_tab = true,
 	window_background_image = wezterm.home_dir .. "/Documents/terminal.jpg",
 	window_background_image_hsb = {
-		brightness = 0.04,
+		brightness = 0.10,
 	},
 	keys = {
 		{
@@ -22,6 +22,70 @@ return {
 			key = "_",
 			mods = "SHIFT|ALT",
 			action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+		},
+		-- Make Option-Left equivalent to Alt-b which many line editors interpret as backward-word
+		{
+			key = "LeftArrow",
+			mods = "OPT",
+			action = wezterm.action({ SendString = "\x1bb" }),
+		},
+		-- Make Option-Right equivalent to Alt-f; forward-word
+		{
+			key = "RightArrow",
+			mods = "OPT",
+			action = wezterm.action({ SendString = "\x1bf" }),
+		},
+		{
+			key = "h",
+			mods = "CTRL|SHIFT",
+			action = wezterm.action({ ActivatePaneDirection = "Left" }),
+		},
+		{
+			key = "j",
+			mods = "CTRL|SHIFT",
+			action = wezterm.action({ ActivatePaneDirection = "Down" }),
+		},
+		{
+			key = "k",
+			mods = "CTRL|SHIFT",
+			action = wezterm.action({ ActivatePaneDirection = "Up" }),
+		},
+		{
+			key = "l",
+			mods = "CTRL|SHIFT",
+			action = wezterm.action({ ActivatePaneDirection = "Right" }),
+		},
+		{
+			key = "LeftArrow",
+			mods = "CTRL|SHIFT",
+			action = wezterm.action({ AdjustPaneSize = { "Left", 1 } }),
+		},
+		{
+			key = "DownArrow",
+			mods = "CTRL|SHIFT",
+			action = wezterm.action({ AdjustPaneSize = { "Down", 1 } }),
+		},
+		{
+			key = "UpArrow",
+			mods = "CTRL|SHIFT",
+			action = wezterm.action({ AdjustPaneSize = { "Up", 1 } }),
+		},
+		{
+			key = "RightArrow",
+			mods = "CTRL|SHIFT",
+			action = wezterm.action({ AdjustPaneSize = { "Right", 1 } }),
+		},
+		{
+			key = "E",
+			mods = "CTRL|SHIFT",
+			action = wezterm.action.PromptInputLine({
+				description = "Enter new name for tab",
+				action = wezterm.action_callback(function(window, pane, line)
+					if line then
+						window:active_tab():set_title(line)
+					end
+				end),
+			}),
 		},
 	},
 }
