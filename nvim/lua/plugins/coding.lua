45483ca504401e0fe3ed320cc32cc324f6f58e8c
asasinmode <skin9470@gmail.com>
2025-02-02 14:46:11 +0100

    feat: disable blink auto brackets

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -31,6 +31,11 @@ return {
 						},
 					},
 				},
+				accept = {
+					auto_brackets = {
+						enabled = false,
+					},
+				},
 				documentation = {
 					auto_show = true,
 					auto_show_delay_ms = 200,
