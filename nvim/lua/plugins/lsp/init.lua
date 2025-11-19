b3902b7b319e6c6e0aa0fa7e7f97bffa5559e17d
asasinmode <skin9470@gmail.com>
2025-11-19 21:54:47 +0100

    feat: simplify vue lsp, disable json ls formatting when eslint

--- nvim/lua/plugins/lsp/init.lua
+++ nvim/lua/plugins/lsp/init.lua
@@ -84,46 +84,7 @@ return {
 						provideFormatter = false,
 					},
 				},
-				vue_ls = {
-					on_init = function(client)
-						client.handlers["tsserver/request"] = function(_, result, context)
-							local ts_clients = vim.lsp.get_clients({ bufnr = context.bufnr, name = "ts_ls" })
-							local vtsls_clients = vim.lsp.get_clients({ bufnr = context.bufnr, name = "vtsls" })
-							local clients = {}
-
-							vim.list_extend(clients, ts_clients)
-							vim.list_extend(clients, vtsls_clients)
-
-							if #clients == 0 then
-								vim.notify(
-									"Could not find `vtsls` or `ts_ls` lsp client, `vue_ls` would not work without it.",
-									vim.log.levels.ERROR
-								)
-								return
-							end
-							local ts_client = clients[1]
-
-							local param = unpack(result)
-							local id, command, payload = unpack(param)
-							ts_client:exec_cmd({
-								title = "vue_request_forward", -- You can give title anything as it's used to represent a command in the UI, `:h Client:exec_cmd`
-								command = "typescript.tsserverRequest",
-								arguments = {
-									command,
-									payload,
-								},
-							}, { bufnr = context.bufnr }, function(_, r)
-								local response = r and r.body
-								-- TODO: handle error or response nil here, e.g. logging
-								-- NOTE: Do NOT return if there's an error or no response, just return nil back to the vue_ls to prevent memory leak
-								local response_data = { { id, response } }
-
-								---@diagnostic disable-next-line: param-type-mismatch
-								client:notify("tsserver/response", response_data)
-							end)
-						end
-					end,
-				},
+				vue_ls = {},
 				vtsls = {
 					settings = {
 						complete_function_calls = true,
@@ -221,7 +182,7 @@ return {
 					Util.lsp.on_attach(function(client, _)
 						if client.name == "eslint" then
 							client.server_capabilities.documentFormattingProvider = true
-						elseif client.name == "vtsls" or client.name == "vue_ls" then
+						elseif client.name == "vtsls" or client.name == "vue_ls" or client.name == "jsonls" then
 							client.server_capabilities.documentFormattingProvider = false
 						end
 					end)
