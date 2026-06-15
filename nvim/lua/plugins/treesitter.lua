413c4055f31f21a92bc3e949bc107997f662db50
asasinmode <>
2026-06-15 18:29:04 +0200

    update treesitter

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,87 +1,52 @@
 return {
 	{
 		"nvim-treesitter/nvim-treesitter",
-		version = false, -- last release is way too old and doesn't work on Windows
-		build = ":TSUpdate",
+		main = "nvim-treesitter",
 		event = { "VeryLazy", "BufReadPost", "BufNewFile", "BufWritePre" },
-		init = function(plugin)
-			-- PERF: add nvim-treesitter queries to the rtp and it's custom query predicates early
-			-- This is needed because a bunch of plugins no longer `require("nvim-treesitter")`, which
-			-- no longer trigger the **nvim-treeitter** module to be loaded in time.
-			-- Luckily, the only thins that those plugins need are the custom queries, which we make available
-			-- during startup.
-			require("lazy.core.loader").add_to_rtp(plugin)
-			require("nvim-treesitter.query_predicates")
-		end,
-		dependencies = {
-			{
-				"nvim-treesitter/nvim-treesitter-textobjects",
-				config = function()
-					-- When in diff mode, we want to use the default
-					-- vim text objects c & C instead of the treesitter ones.
-					local move = require("nvim-treesitter.textobjects.move") ---@type table<string,fun(...)>
-					local configs = require("nvim-treesitter.configs")
-					for name, fn in pairs(move) do
-						if name:find("goto") == 1 then
-							move[name] = function(q, ...)
-								if vim.wo.diff then
-									local config = configs.get_module("textobjects.move")[name] ---@type table<string,string>
-									for key, query in pairs(config or {}) do
-										if q == query and key:find("[%]%[][cC]") then
-											vim.cmd("normal! " .. key)
-											return
-										end
-									end
-								end
-								return fn(q, ...)
-							end
-						end
-					end
-				end,
-			},
-		},
+		build = ":TSUpdate",
 		cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
-		---@type TSConfig
-		---@diagnostic disable-next-line: missing-fields
-		opts = {
-			highlight = { enable = true },
-			indent = { enable = true },
-			ensure_installed = {
-				"bash",
-				"c",
+		config = function()
+			require("nvim-treesitter.install").compilers = { "gcc" }
+			require("nvim-treesitter").setup({
+				install_dir = vim.fn.stdpath("data") .. "/site",
+			})
+			vim.api.nvim_create_autocmd("FileType", {
+				callback = function()
+					pcall(vim.treesitter.start)
+					vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+				end,
+			})
+			require("nvim-treesitter").install({
 				"css",
 				"diff",
 				"html",
 				"javascript",
 				"json",
 				"json5",
-				"jsonc",
 				"lua",
 				"luap",
 				"markdown",
 				"markdown_inline",
 				"query",
 				"regex",
-				"rust",
 				"sql",
 				"typescript",
 				"vim",
 				"vimdoc",
 				"vue",
 				"yaml",
-			},
-			incremental_selection = {
-				enable = true,
-				keymaps = {
-					init_selection = "<C-space>",
-					node_incremental = "<C-space>",
-					scope_incremental = "<nop>",
-					node_decremental = "<bs>",
-				},
-			},
-		},
-		config = function(_, opts)
-			require("nvim-treesitter.configs").setup(opts)
+				"astro",
+			})
 		end,
+	},
+
+	{
+		"nvim-treesitter/nvim-treesitter-textobjects",
+		branch = "main",
+		event = "VeryLazy",
+		-- config = function(_, opts)
+		-- 	local TS = require("nvim-treesitter-textobjects")
+		-- 	TS.setup(opts)
+		-- end,
 	},
 }
